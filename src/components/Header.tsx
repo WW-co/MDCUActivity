298d0d2f4836c611513661e5c036e30e6c19ba50
jininn <melonsugar01234@gmail.com>
2024-06-13 13:28:20 +0700

    dashboard

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ function Header() {
   return (
     <header className="bg-purple-800 text-white flex shadow-xl h-16 items-center px-8 gap-8 mb-8">
       <h1 className="font-bold text-3xl">MDCU ACTIVITY</h1>
-      <ul className="flex gap-4 text-md text-white">
+      <ul className="flex gap-8 text-md text-white">
         <li>
           <Link
             href="/"
@@ -34,7 +34,7 @@ function Header() {
             href="/myact"
             className="underline underline-offset-4 hover:no-underline"
           >
-            กิจกกรรมของฉัน
+            กิจกรรมของฉัน
           </Link>
         </li>
       </ul>
