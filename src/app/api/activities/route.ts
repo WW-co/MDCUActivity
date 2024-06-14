e7f37fe9498dc79c34e18831a4d93441da2dbe6f
jininn <melonsugar01234@gmail.com>
2024-06-14 11:40:34 +0700

    style: remove comment

--- src/app/api/activities/route.ts
+++ src/app/api/activities/route.ts
@@ -6,8 +6,6 @@ import { activity } from "@/db/schema";
 export async function GET(request: Request, { params }: { params: { id: number } }) {
   const _activity = await db.select().from(activity);
 
-  //const filteredActivity = _activity.filter(activity => activity.id == params.id);
-
   return Response.json({ activities: _activity });
 }
 
